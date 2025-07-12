8b4021039de9cfe4531ccd77a7099f40ed216f8f
RaniaSabryQC <rania.sabry@outlook.com>
2025-07-13 02:35:37 +0300

    Initial Commit
    first create project

---
+++ src/test/java/workshopTasks/Task1.java
@@ -0,0 +1,12 @@
+package workshopTasks;
+
+import org.testng.annotations.Test;
+
+public class Task1 {
+    @Test
+    public void testTask1() {
+        // This is a placeholder for the actual test code.
+        // You can implement your test logic here.
+        System.out.println("Running Task 1 test");
+    }
+}
