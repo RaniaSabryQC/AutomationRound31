7dabf22ff42f15bb098d0d057470470bb29c679f
RaniaSabryQC <rania.sabry@outlook.com>
2025-07-15 01:44:31 +0300

    Updating Read me

--- README.md
+++ README.md
@@ -2,3 +2,21 @@
 Test Automation Learning Round 31
 
 This is the test change
+
+# Convention
+- lowerCamelCase
+- UpperCamelCase 
+- lower_snake_case
+- UPPER_SNAKE_CASE
+
+# Project Structure
+.src
+    ├─main this is the main source set
+        java
+        resources
+    └─test this is the test source set
+        java
+        resources
+
+
+# apply first Task
---
+++ src/test/java/workshopTasks/Task1Test.java
@@ -0,0 +1,24 @@
+package workshopTasks;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Task1Test {
+    @Test
+    public void testTask1() {
+//        Task 1: Open Google Chrome, navigate to [https://duckduckgo.com/], assert that the page title is [Google], and close Google Chrome.
+//        Steps:
+//       - Open Google Chrome
+//       - Navigate to [https://duckduckgo.com/]
+//        -Assert that the page title is [Google]
+//        -Close Google Chrome
+
+        WebDriver driver;
+        driver = new ChromeDriver();
+        driver.navigate().to("https://duckduckgo.com/");
+        Assert.assertEquals(driver.getTitle(),"Google");
+        driver.quit();
+    }
+}
